c42cee7e022737482f162c8dc0f49fb814aa0eac
417MahirRahman <35mahirrahman@gmail.com>
2026-07-08 16:53:31 +0600

    catchAsync and sendResponse utils added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,31 +1,23 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
+import { sendResponse } from "../../utils/sendResponse";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-    const user = await userService.createUserIntoDB(payload);
-     res.status(httpStatus.CREATED).json({
-       success: true,
-       statusCode: httpStatus.CREATED,
-       message: "User registered successfully",
-       data: {
-         user,
-       },
-     });
-   
-  } catch (error: any) {
-    console.log(error);
 
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-      message: "Failed to register user",
-      error: (error as Error).message,
+    const user = await userService.createUserIntoDB(payload);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User registered successfully",
+      data: { user },
     });
-  }
-};
+  },
+);
 
 export const userController = {
   createUser,
